9f13d2ef8969347de7ea04d385a051cafd3f4b85
Justin John <34035011+justinjohn0306@users.noreply.github.com>
2025-01-14 18:06:53 +0530

    Update load_model.py

--- core/utils/load_model.py
+++ core/utils/load_model.py
@@ -1,29 +1,39 @@
+import os
+
 def load_model(model_path: str, device: str = "cuda", **kwargs):
     if kwargs.get("force_ori_type", False):
-        # for hubert, landmark, retinaface, mediapipe
+        # For hubert, landmark, retinaface, mediapipe
         model = load_force_ori_type(model_path, device, **kwargs)
         return model, "ori"
 
     if model_path.endswith(".onnx"):
-        # onnx
+        # ONNX
         import onnxruntime
 
-        if device == "cuda":
-            providers = ["CUDAExecutionProvider", "CPUExecutionProvider"]
-        else:
-            providers = ["CPUExecutionProvider"]
+        providers = ["CUDAExecutionProvider", "CPUExecutionProvider"] if device == "cuda" else ["CPUExecutionProvider"]
         model = onnxruntime.InferenceSession(model_path, providers=providers)
         return model, "onnx"
 
     elif model_path.endswith(".engine") or model_path.endswith(".trt"):
-        # tensorRT
+        # TensorRT
         from .tensorrt_utils import TRTWrapper
+
+        if not os.path.isfile(model_path):
+            # Try FP16 as a fallback
+            alt_model_path = model_path.replace("_fp32", "_fp16")
+            if os.path.isfile(alt_model_path):
+                print(f"FP32 engine not found. Falling back to FP16 engine: {alt_model_path}")
+                model_path = alt_model_path
+            else:
+                raise FileNotFoundError(
+                    f"Neither FP32 nor FP16 engine file found: {model_path} or {alt_model_path}"
+                )
 
         model = TRTWrapper(model_path)
         return model, "tensorrt"
 
     elif model_path.endswith(".pt") or model_path.endswith(".pth"):
-        # pytorch
+        # PyTorch
         model = create_model(model_path, device, **kwargs)
         return model, "pytorch"
 
@@ -40,10 +50,7 @@ def create_model(
 ):
     import importlib
 
-    # module = getattr(importlib.import_module('..models.modules', __package__), module_name)
     module = getattr(importlib.import_module(package_name, __package__), module_name)
-    # from <package_name> import <module_name>
-
     model = module(**kwargs)
     model.load_model(model_path).to(device)
     return model
@@ -54,7 +61,7 @@ def load_force_ori_type(
     device: str = "cuda",
     module_name="",
     package_name="..aux_models.modules",
-    force_ori_type=False, 
+    force_ori_type=False,
     **kwargs,
 ):
     import importlib
